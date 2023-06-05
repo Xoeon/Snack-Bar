866573f067b5fd4617fe14be0b0589053d75fbcc
Xoeon <jayy_19@ewhain.net>
2023-06-05 17:21:39 +0900

    간식 신청하기 기능 추가

--- snack/urls.py
+++ snack/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 app_name = 'snack'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('snack/request_create/', views.snack_request_create,
+         name='snack_request_create'),
 ]
--- snack/views.py
+++ snack/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Snack
+from .forms import SnackForm
 
 
 def index(request):
     snack_request_list = Snack.objects.order_by('-create_date')
     context = {'snack_request_list': snack_request_list}
     return render(request, 'snack/snack_request_list.html', context)
+
+
+def snack_request_create(request):
+    form = SnackForm()
+    return render(request, 'snack/snack_request_create.html', {'form': form})
